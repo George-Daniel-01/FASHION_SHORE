fa35906a9f7471e9b29515a4151227befeb15a9e
George Daniel <georgeabiamakadaniel@gmail.com>
2025-11-15 23:13:43 +0100

    update

--- Frontend/src/components/Amin/AdminSidebar.jsx
+++ Frontend/src/components/Amin/AdminSidebar.jsx
@@ -58,7 +58,7 @@ export const AdminSidebar = () => {
         </NavLink>
 
         <NavLink
-          to="/admin/store"
+          to="/"
           className={({ isActive }) =>
             isActive
               ? "bg-gray-700 text-white py-3 px-4 rounded flex items-center space-x-2"
